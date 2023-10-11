b92ca2505dcbc051aefdeedb84ded5977529c795
Lenny139 <lenny.rincon.5@gmail.com>
2023-10-11 00:39:42 -0500

    funciones relacionada a JSON centralizadas

--- src/module-info.java
+++ src/module-info.java
@@ -1,12 +1,2 @@
-/**
- * 
- */
-/**
- * 
- */
-
 module BeltonGym {
-		requires com.fasterxml.jackson.datatype.jsr310;
-	  requires com.fasterxml.jackson.databind;
-	  exports co.edu.upb.BeltonGym;
-	}
+}
